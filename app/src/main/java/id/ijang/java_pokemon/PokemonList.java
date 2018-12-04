c43f80f12a944f0e7036ab5ce1da32b6e7edc2c4
muhammadijang <muhammadrizal@if.undip.ac.id>
2018-12-04 16:56:04 +0000

    Add blank fragment untuk menampilkan list pokemon

---
+++ app/src/main/java/id/ijang/java_pokemon/PokemonList.java
@@ -0,0 +1,29 @@
+package id.ijang.java_pokemon;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class PokemonList extends Fragment {
+
+
+    public PokemonList() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_pokemon_list, container, false);
+    }
+
+}
