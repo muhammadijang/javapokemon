9e37c3a091c5930d9a9e93b7b0be5c1eb35ca2be
muhammadijang <muhammadrizal@if.undip.ac.id>
2018-12-05 08:46:50 +0000

    Membuat recyclerview dapat diclick (memunculkan toast)

--- app/src/main/java/id/ijang/java_pokemon/Adapter/PokemonListAdapter.java
+++ app/src/main/java/id/ijang/java_pokemon/Adapter/PokemonListAdapter.java
@@ -8,11 +8,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
 import java.util.List;
 
+import id.ijang.java_pokemon.Interface.IItemClickListener;
 import id.ijang.java_pokemon.Model.Pokemon;
 import id.ijang.java_pokemon.R;
 
@@ -39,6 +41,14 @@ public class PokemonListAdapter extends RecyclerView.Adapter<PokemonListAdapter.
         Glide.with(context).load(pokemonList.get(position).getImg()).into(holder.pokemon_image);
         //Get Name
         holder.pokemon_name.setText(pokemonList.get(position).getName());
+
+        //Event
+        holder.setiItemClickListener(new IItemClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                Toast.makeText(context, "Click at Pokemon: "+pokemonList.get(position).getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
@@ -46,16 +56,29 @@ public class PokemonListAdapter extends RecyclerView.Adapter<PokemonListAdapter.
         return pokemonList.size();
     }
 
-    public class MyViewHolder extends RecyclerView.ViewHolder {
+    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         ImageView pokemon_image;
         TextView pokemon_name;
+
+        IItemClickListener iItemClickListener;
+
+        public void setiItemClickListener(IItemClickListener iItemClickListener) {
+            this.iItemClickListener = iItemClickListener;
+        }
 
         public MyViewHolder(View itemView) {
             super(itemView);
 
             pokemon_image = (ImageView)itemView.findViewById(R.id.pokemon_image);
             pokemon_name = (TextView)itemView.findViewById(R.id.txt_pokemon_name);
+
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            iItemClickListener.onClick(view,getAdapterPosition());
         }
     }
 }
